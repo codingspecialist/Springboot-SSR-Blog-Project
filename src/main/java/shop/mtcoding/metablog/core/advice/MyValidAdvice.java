b51a7b248b5dbccb7ac4dc8d7d10b82508911739
metacoding <getinthere@naver.com>
2023-04-19 18:18:09 +0900

    API 익세션 ì셉리와 ADVICE 완료

--- src/main/java/shop/mtcoding/metablog/core/advice/MyValidAdvice.java
+++ src/main/java/shop/mtcoding/metablog/core/advice/MyValidAdvice.java
@@ -1,4 +1,33 @@
 package shop.mtcoding.metablog.core.advice;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import shop.mtcoding.metablog.core.exception.Exception400;
+
+@Aspect
+@Component
 public class MyValidAdvice {
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+    public void postMapping() {
+    }
+
+    @Before("postMapping()")
+    public void validationAdvice(JoinPoint jp) {
+        Object[] args = jp.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof Errors) {
+                Errors errors = (Errors) arg;
+                if (errors.hasErrors()) {
+                    throw new Exception400(
+                            errors.getFieldErrors().get(0).getField(),
+                            errors.getFieldErrors().get(0).getDefaultMessage()
+                    );
+                }
+            }
+        }
+    }
 }
