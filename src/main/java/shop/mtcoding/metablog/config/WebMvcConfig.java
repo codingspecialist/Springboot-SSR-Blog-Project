c044e10dde5cb80ad84901e7ab51dbdfcc51eeab
metacoding <getinthere@naver.com>
2023-04-19 18:12:11 +0900

    사진 파일 절대 경로 상대경로 세팅

--- src/main/java/shop/mtcoding/metablog/config/WebMvcConfig.java
+++ src/main/java/shop/mtcoding/metablog/config/WebMvcConfig.java
@@ -1,4 +1,26 @@
 package shop.mtcoding.metablog.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+
+    // CORS
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        // 1. 절대경로 file:///c:/upload/
+        // 2. 상대경로 file:./upload/
+        registry
+                .addResourceHandler("/upload/**")
+                .addResourceLocations("file:" + "./upload/")
+                .setCachePeriod(60 * 60) // 초 단위 => 한시간
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
 }
