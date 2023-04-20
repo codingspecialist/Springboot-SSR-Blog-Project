52e19101e7c109af8967d3d34911fce8c283a7e7
metacoding <getinthere@naver.com>
2023-04-20 22:26:33 +0900

    로그인 편하게 하려고 ssar 유저 더미 만들었음

--- src/main/java/shop/mtcoding/metablog/MetablogApplication.java
+++ src/main/java/shop/mtcoding/metablog/MetablogApplication.java
@@ -1,13 +1,31 @@
 package shop.mtcoding.metablog;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import shop.mtcoding.metablog.model.user.User;
+import shop.mtcoding.metablog.model.user.UserRepository;
 
 @SpringBootApplication
 public class MetablogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MetablogApplication.class, args);
-	}
+    @Bean
+    CommandLineRunner init(BCryptPasswordEncoder passwordEncoder, UserRepository userRepository) {
+        return args -> {
+            User ssar = User.builder()
+                    .username("ssar")
+                    .password(passwordEncoder.encode("1234"))
+                    .email("ssar@nate.com")
+                    .build();
+            userRepository.save(ssar);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetablogApplication.class, args);
+    }
 
 }
